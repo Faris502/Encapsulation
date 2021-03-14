66adc156fc20390e59addd1ba34dd022bd9a22f7
Faris502 <78650736+Faris502@users.noreply.github.com>
2021-03-14 09:31:04 +0300

    Delete Main3.java

--- Class8-1/src/Main3.java
+++
@@ -1,9 +0,0 @@
-
-public class Main3 {
-
-	public static void main(String[] args) {
-		Encapsulation myObj = new Encapsulation();
-	    myObj.setAcademicID("439100211");
-	    System.out.println(myObj.getAcademicID());
-	  }
-}
