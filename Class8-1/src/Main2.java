7a7b627d91012643cf4059977c755bb373c964d8
Faris502 <78650736+Faris502@users.noreply.github.com>
2021-03-14 09:30:59 +0300

    Delete Main2.java

--- Class8-1/src/Main2.java
+++
@@ -1,9 +0,0 @@
-
-public class Main2 {
-
-	public static void main(String[] args) {
-		Encapsulation myObj = new Encapsulation();
-	    myObj.setPassword("f12345");
-	    System.out.println(myObj.getPassword());
-	  }
-}
